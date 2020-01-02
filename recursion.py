cf03f98f49f1657718576a11d509b4903107c482
Christopher Lamm <chris@theantichris.com>
2020-01-02 07:29:50 -0500

    looping through values to build the call stack

--- recursion.py
+++ recursion.py
@@ -2,4 +2,12 @@ def sum_to_one(n):
     result = 1
     call_stack = []
 
+    while n is not 1:
+        execution_context = {"n_value": n}
+        call_stack.append(execution_context)
+        n -= 1
+        print(call_stack)
+
     return result, call_stack
+
+sum_to_one(4)
